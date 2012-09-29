8c11ac9848c180c3068e7859c9fe374a4d68ef6d
Tim <Tim@Tim-PC>
2012-09-29 15:22:20 +1000

    dot dot dot

--- micro/src/common.c
+++ micro/src/common.c
@@ -12,12 +12,23 @@
 int step = 0;
 char* string;
 int count = 0;
+int old = 0;
 void setup() {
+    DDRE = 0x00;
     setupSerial();
+    setupPins(); 
+    DEBUG_PRINT("START\n");
+/*
+    
     setupPins(); 
     setupArray();
     setupClock();
     setupControl();
+*/
 }
 
-void loop() {}
+void loop() {
+    int new = PINE & 0x10;
+    DEBUG_PRINT("Changed: %i\n", new);
+    _delay_ms(1000);
+}
--- micro/src/common.h
+++ micro/src/common.h
@@ -10,6 +10,7 @@
     #include <stdio.h>
     #define DEBUG
     #define TEST
+    #define ARDUINO
  
     #ifdef DEBUG
         #define DEBUG_PRINT(a, ...) printf(a, ##__VA_ARGS__)
@@ -37,19 +38,38 @@
         #define GIMSK_def GIMSK
         #define INT0_def INT0
         #define F_CPU 16000000UL
-        #define LATCH1_PORT PORTA
-        #define LATCH1_DDR DDRA
-        #define LATCH2_PORT PORTC
-        #define LATCH2_DDR DDRC
-        #define LATCH1_MEMORY 0x03
-        #define LATCH2_MEMORY 0x03
-        #define LATCH1_CLEAR 0x02
-        #define LATCH2_CLEAR 0x02
-        #define LATCH1_SET 0x01
-        #define LATCH2_SET 0x01
-        #define EICR_def EICRB
+        #define LATCH_PORT1 PORTB
+        #define LATCH1_DDR DDRB
+        #define LATCH_PORT2 PORTD
+        #define LATCH2_DDR DDRD
+        #define LATCH1_MEMORY 0x30
+        #define LATCH2_MEMORY 0xA0
+        #define LATCH1_CLEAR 0x10
+        #define LATCH2_CLEAR 0x20
+        #define LATCH1_SET 0x20
+        #define LATCH2_SET 0x80
+        #define EICR_def EICRA
         #define EIMSK_def EIMSK
-        
+        #define EICR_value 0x05
+        #define EIMSK_IR_VALUE 0x01
+        #define EIMSK_OPTIC_VALUE 0x02
+        #define SENSOR_PIN PIND
+        #define SENSOR_OPTICAL_MASK 0x08
+        #define LATCH1_DDRMASK 0x37
+        #define LATCH2_DDRMASK 0xE0
+        #define ALARM_LED_PORT PORTC
+        #define ALARM_BUZZER_PORT PORTC
+        #define ALARM_LED_VALUE 0x10
+        #define ALARM_BUZZER_VALUE 0x20
+        #define PM_LED_VALUE 0x08
+        #define PM_LED_PORT PORTC
+        #define WEATHER_PORT PORTC
+        #define WEATHER_CLEAR_MASK 0x07
+        #define WEATHER_FINE 0x01
+        #define WEATHER_CLOUDY 0x02
+        #define WEATHER_RAINY 0x04
+        #define SENSOR_PORT PORTD
+        #define SENSOR_DDR_MASK 0x0C
     #else
         #define UBRRH_def UBRR0H
         #define UBRRL_def UBRR0L
