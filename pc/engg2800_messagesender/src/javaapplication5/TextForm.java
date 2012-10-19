e1dfed9b381f04782c09a2d55d6f9dfe3d380dc1
Michael Banfield <shrapnelbikes@gmail.com>
2012-10-19 14:59:02 +1000

    changed timestamp in minutes

--- pc/engg2800_messagesender/src/javaapplication5/TextForm.java
+++ pc/engg2800_messagesender/src/javaapplication5/TextForm.java
@@ -17,8 +17,9 @@ import javax.swing.JTextField;
  * @author Michael
  */
 class TextForm extends JPanel {
+
     private JTextField[] fields;
-    
+
     public TextForm(String[] labels, char[] mnemonics, int[] widths, String[] tips) {
         super(new BorderLayout());
         JPanel labelPanel = new JPanel(new GridLayout(labels.length, 1));
@@ -28,53 +29,56 @@ class TextForm extends JPanel {
         fields = new JTextField[labels.length];
 
         for (int i = 0; i < labels.length; i += 1) {
-          fields[i] = new JTextField();
-          if (i < tips.length)
-            fields[i].setToolTipText(tips[i]);
-          if (i < widths.length)
-            fields[i].setColumns(widths[i]);
+            fields[i] = new JTextField();
+            if (i < tips.length) {
+                fields[i].setToolTipText(tips[i]);
+            }
+            if (i < widths.length) {
+                fields[i].setColumns(widths[i]);
+            }
 
-          JLabel lab = new JLabel(labels[i], JLabel.RIGHT);
-          lab.setLabelFor(fields[i]);
-          if (i < mnemonics.length)
-            lab.setDisplayedMnemonic(mnemonics[i]);
+            JLabel lab = new JLabel(labels[i], JLabel.RIGHT);
+            lab.setLabelFor(fields[i]);
+            if (i < mnemonics.length) {
+                lab.setDisplayedMnemonic(mnemonics[i]);
+            }
 
-          labelPanel.add(lab);
-          JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
-          p.add(fields[i]);
-          fieldPanel.add(p);
+            labelPanel.add(lab);
+            JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            p.add(fields[i]);
+            fieldPanel.add(p);
         }
     }
-    
+
     public ArrayList<Integer> getBinary(int hour, int minute, int wordLength) {
         ArrayList<Integer> list = new ArrayList();
-          int hourInt;
-          int minuteInt;
-          if(fields[hour].getText().equals("")) {
-              hourInt = 0;
-          } else {
-              hourInt = Integer.parseInt(fields[hour].getText());
-          }
-          if(fields[minute].getText().equals("")) {
-              minuteInt = 0;
-          } else {
-              minuteInt = Integer.parseInt(fields[minute].getText());
-          }
-          int timeStamp = hourInt * 60 * 60 + minuteInt * 60;
-          System.out.println("The timestamp is " + timeStamp);
-          String binary = Integer.toBinaryString(timeStamp);
-          System.out.println(binary);
-          char charArray[] = binary.toCharArray();
-          int count = wordLength - charArray.length;
-          int c = 0;
-          while(c < count) {
-              list.add(0);
-              c++;
-          }
-          for(char s: charArray){
-              list.add(Character.getNumericValue(s));
+        int hourInt;
+        int minuteInt;
+        if (fields[hour].getText().equals("")) {
+            hourInt = 0;
+        } else {
+            hourInt = Integer.parseInt(fields[hour].getText());
+        }
+        if (fields[minute].getText().equals("")) {
+            minuteInt = 0;
+        } else {
+            minuteInt = Integer.parseInt(fields[minute].getText());
+        }
+        int timeStamp = hourInt * 60 + minuteInt;
+        System.out.println("The timestamp is" + timeStamp);
+        String binary = Integer.toBinaryString(timeStamp);
+        char charArray[] = binary.toCharArray();
+        int count = wordLength - charArray.length;
+        int c = 0;
+        while (c < count) {
+            list.add(0);
+            c++;
+        }
+        for (char s : charArray) {
+            list.add(Character.getNumericValue(s));
 
-          }
-          return list;
+        }
+        System.out.println("The nice timestamp is " + list.toString());
+        return list;
     }
 }
